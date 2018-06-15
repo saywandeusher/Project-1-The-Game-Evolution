dd41ea7fe1f0ba363d8d4b62f40d2455a988413e
saywandeusher <ongsaywan@hotmail.com>
2018-06-16 02:56:44 +0800

    Created directional keys

--- script.js
+++ script.js
@@ -27,17 +27,41 @@ function distance(x1, y1, x2, y2) {
     return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
 }
 
+function colorText(txt, fnt, x, y, color) {
+    ctx.font = fnt;
+    ctx.fillStyle = color;
+    ctx.fillText(txt, x, y);
+}
+
+function fadeOut(text, x, y) {
+    var a = 1.0,   // full opacityle
+        interval = setInterval(function () {
+            canvas.width = canvas.width; // Cars the canvas
+            ctx.fillStyle = "rgba(0, 0, 255, " + a + ")";
+            ctx.font = "italic 20pt Arial";
+            ctx.fillText(text, x, y);
+            a = a - 0.03; // decrease opacity (fade out)
+            if (a < 0) {
+                canvas.width = canvas.width;
+                clearInterval(interval);
+            }
+        }, 50); 
+}
+
+
+
 //user's keydown
 score = 0;
 
 document.onkeydown = function (e) {
     switch (e.key) {
         case 'ArrowUp':
-            if (circles[0] && circles[0].radius < 17) {
+            if (circles[0] && circles[0].direction == 1 && circles[0].radius < 17) {
                 score = score + 3;
                 console.log("+3");
                 circles.shift();
-            }else if (circles[0] && circles[0].radius < 30 && circles[0].radius > 16) { 
+
+            }else if (circles[0] && circles[0].direction == 1 && circles[0].radius > 16 && circles[0].radius < 25 ) { 
                 score = score + 1;
                 console.log("+1");
                 circles.shift();
@@ -46,68 +70,173 @@ document.onkeydown = function (e) {
                 circles.shift();
             }
             break;
-        case 'ArrowDown':
-            circles[1].color = 'red';
-            score = score + 1;
-            console.log("+1")
-            break;
+
         case 'ArrowLeft':
-            circles[2].color = 'red';
-            score = score + 1;
-            console.log("+1")
+            if (circles[0] && circles[0].direction == 2 && circles[0].radius < 17) {
+                score = score + 3;
+                console.log("+3");
+                circles.shift();
+            }else if (circles[0] && circles[0].direction == 2 && circles[0].radius > 16 && circles[0].radius < 25 ) { 
+                score = score + 1;
+                console.log("+1");
+                circles.shift();
+            }else{
+                console.log('missed')
+                circles.shift();
+            }
             break;
+
         case 'ArrowRight':
-            circles[3].color = 'red';
-            score = score + 1;
-            console.log("+1")
+            if (circles[0] && circles[0].direction == 3 && circles[0].radius < 17) {
+                score = score + 3;
+                console.log("+3");
+                circles.shift();
+            }else if (circles[0] && circles[0].direction == 3 && circles[0].radius > 16 && circles[0].radius < 25 ) { 
+                score = score + 1;
+                console.log("+1");
+                circles.shift();
+            }else{
+                console.log('missed')
+                circles.shift();
+            }
+            break;
+
+        case 'ArrowDown':
+            if (circles[0] && circles[0].direction == 4 && circles[0].radius < 17) {
+                score = score + 3;
+                console.log("+3");
+                circles.shift();
+            }else if (circles[0] && circles[0].direction == 4 && circles[0].radius > 16 && circles[0].radius < 25 ) { 
+                score = score + 1;
+                console.log("+1");
+                circles.shift();
+            }else{
+                console.log('missed')
+                circles.shift();
+            }
+            break;
     }
 };
 
-// Objects
+
+// creates circle
 function Circle(x, y, radius, color) {
     this.x = x;
     this.y = y;
     this.radius = radius;
     this.color = color;
+    this.direction = Math.floor(Math.random() * 4) + 1;
 
     this.update = function() {
         this.draw();
         this.draw2();
-        this.drawArrow();
         this.radius -= 0.4;
-        
+        this.randomizeArrow();
     };
 
+
     this.draw = function() {
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = 'red';
-    ctx.stroke();
-    ctx.closePath();
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = 'red';
+        ctx.stroke();
+        ctx.closePath();
     };
 
     this.draw2 = function() {
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, 15, 0, Math.PI * 2, false);
-    ctx.strokeStyle = this.color;
-    ctx.stroke();
-    ctx.closePath();
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, 15, 0, Math.PI * 2, false);
+        ctx.strokeStyle = this.color;
+        ctx.stroke();
+        ctx.closePath();
     };
 
-    this.drawArrow = function() {
-    ctx.beginPath();
-    ctx.moveTo(this.x + 4, this.y + 1);
-    ctx.lineTo(this.x - 3, this.y + 1);
-    ctx.lineTo(this.x , this.y - 6);
-    ctx.fill();
-    ctx.moveTo(this.x, this.y + 1);
-    ctx.lineTo(this.x, this.y + 8);
-    ctx.strokeStyle = 'black';
-    ctx.stroke();
-    ctx.closePath();
+    this.randomizeArrow = function() {
+            if (this.direction == 1) {
+                var up = this.upArrow();
+            }else if (this.direction == 2) {
+                var left = this.leftArrow();
+            }else if (this.direction == 3) {
+                var right = this.rightArrow();
+            }else if (this.direction == 4) {
+                var down = this.downArrow();
+            }
     }
-}
+
+    this.upArrow = function() {
+        ctx.beginPath();
+    //right(highernum go right , highernum go down)
+        ctx.moveTo(this.x + 4, this.y + 1);
+    //left(highernum go right , highernum go down)
+        ctx.lineTo(this.x - 3, this.y + 1);
+    //center(highernum go right , highernum go down)
+        ctx.lineTo(this.x , this.y - 6);
+        ctx.fill();
+    //top(highernum go right , highernum go down)
+        ctx.moveTo(this.x, this.y + 1);
+    //bottom(highernum go right , highernum go down)
+        ctx.lineTo(this.x, this.y + 8);
+        ctx.strokeStyle = 'black';
+        ctx.stroke();
+        ctx.closePath();
+    }
+
+    this.leftArrow = function() {
+        ctx.beginPath();
+    //top(highernum go right , highernum go down)
+        ctx.moveTo(this.x , this.y - 4);
+    //center(highernum go right , highernum go down)
+        ctx.lineTo(this.x - 8, this.y);
+    //bottom(highernum go right , highernum go down)
+        ctx.lineTo(this.x , this.y + 4);
+        ctx.fill();
+    //top(highernum go right , highernum go down)
+        ctx.moveTo(this.x -3, this.y );
+    //bottom(highernum go right , highernum go down)
+        ctx.lineTo(this.x +7, this.y );
+        ctx.strokeStyle = 'black';
+        ctx.stroke();
+        ctx.closePath();
+    }
+
+    this.rightArrow = function() {
+        ctx.beginPath();
+    //top(highernum go right , highernum go down)
+        ctx.moveTo(this.x + 1, this.y - 4);
+    //center(highernum go right , highernum go down)
+        ctx.lineTo(this.x + 8, this.y);
+    //bottom(highernum go right , highernum go down)
+        ctx.lineTo(this.x + 1, this.y + 4);
+        ctx.fill();
+    //top(highernum go right , highernum go down)
+        ctx.moveTo(this.x -7, this.y );
+    //bottom(highernum go right , highernum go down)
+        ctx.lineTo(this.x +7, this.y );
+        ctx.strokeStyle = 'black';
+        ctx.stroke();
+        ctx.closePath();
+    }
+
+    this.downArrow = function() {
+        ctx.beginPath();
+    //right(highernum go right , highernum go down)
+        ctx.moveTo(this.x + 5, this.y + 1);
+    //left(highernum go right , highernum go down)
+        ctx.lineTo(this.x - 4, this.y + 1);
+    //center(highernum go right , highernum go down)
+        ctx.lineTo(this.x + 0.5 , this.y + 10);
+        ctx.fill();
+    //top(highernum go right , highernum go down)
+        ctx.moveTo(this.x, this.y - 8);
+    //bottom(highernum go right , highernum go down)
+        ctx.lineTo(this.x, this.y );
+        ctx.strokeStyle = 'black';
+        ctx.stroke();
+        ctx.closePath();
+    }
+
+}   
 
 // Create circles at random location
 let circles;
@@ -122,7 +251,7 @@ function init() {
 
         if(i !== 0) {
             for (let j = 0; j < circles.length; j++) {
-                if (distance(x, y, circles[j].x, circles[j].y) - 15 * 2 < 0) {
+                if (distance(x, y, circles[j].x, circles[j].y) - 50 * 2 < 0) {
                     x = randomIntFromRange(50, canvas.width - 30);
                     y = randomIntFromRange(50, canvas.height - 30); 
 
@@ -131,12 +260,12 @@ function init() {
             }
         }
         circles.push(new Circle(x, y, radius, color));
+
     }
-    console.log('why did this ran twice?')
 }
 
 //makes the circle appear every 1sec
-var turnOnInitial;
+let turnOnInitial;
 
 setInterval(function(){
     turnOnInitial = false;
@@ -152,15 +281,14 @@ function animategame() {
         turnOnInitial = init();
     }
 
-    //creates the animation for the circle
+    // creates the animation for the circle
     circles.forEach(function (item){
     item.update();
     })
 
     //remove circle on it's own
     for (i = 0; i < circles.length; i++) {
-        if (circles[i] && circles[i].radius < 15) {
-
+        if (circles[i] && circles[i].radius < 13) {
             circles.shift();
         }
     }
@@ -189,34 +317,13 @@ function startMenu() {
     menu.style.display = 'block';
 }
 
-function colorText(txt, fnt, x, y, color) {
-    ctx.font = fnt;
-    ctx.fillStyle = color;
-    ctx.fillText(txt, x, y);
-}
-
-function fadeOut(text) {
-    var a = 1.0,   // full opacityle
-        interval = setInterval(function () {
-            canvas.width = canvas.width; // Cars the canvas
-            ctx.fillStyle = "rgba(0, 0, 255, " + a + ")";
-            ctx.font = "italic 20pt Arial";
-            ctx.fillText(text, 210, 300);
-            a = a - 0.03; // decrease opacity (fade out)
-            if (a < 0) {
-                canvas.width = canvas.width;
-                clearInterval(interval);
-            }
-        }, 50); 
-}
-
 function showCanvas() {
     menu.style.display = 'none';
     canvas.style.display = 'block';
     backgroundImg1.style.display = 'block';
     setTimeout(function(){animategame()},3000); 
     // areYouReady();
-    fadeOut('Are you Ready?');
+    fadeOut('Are you Ready?', 210, 300);
     setTimeout(function(){colorText("Go!", '30px Comic Sans MS', 280, 300, 'red')},2000);    
 }
 
